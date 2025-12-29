019d91a0049754087a50efdae1dd38bf5fe7762d
Nurmuhammadov Hojiakbar <x.nurmuxammadov@uzinfocom.uz>
2025-12-29 12:34:23 +0500

    feat: initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,41 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Pattern",
+      description:
+        "JavaScript, React va Vue uchun zamonaviy frontend patternlar. Arxitektura, best practice va real loyihalar uchun amaliy qo‘llanma.",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/NurmuhammadovHojiakbar/patterns",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Kirish",
+          items: [
+            {
+              label: "Kirish",
+              slug: "javascript/introduction",
+            },
+          ],
+        },
+        {
+          label: "Design Patterns",
+          items: [
+            {
+              label: "Singleton Pattern",
+              slug: "javascript/design/singleton",
+            },
+          ],
+        },
+      ],
+    }),
+  ],
+});
